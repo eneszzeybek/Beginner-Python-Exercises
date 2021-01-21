26de774004f069bf1082dd45806f9beb1b633d88
Enes Zeybek <63055251+eneszzeybek@users.noreply.github.com>
2021-01-22 00:38:36 +0300

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -5,8 +5,8 @@ Created on Sun Jan 10 15:09:26 2021
 @author: Enes Zeybek
 """
 
-import numpy as np
-number = np.random.randint(1,10) 
+import random
+number = random.randint(1,9) 
 while True:
     guess_number = int(input("Guess the number between 1-9: "))
     if guess_number == number:
